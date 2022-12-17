a8c1d23237c6ff7f41f21b85191575393d5ec257
ahmadbackend <ahmadbasherjsphp@gmail.com>
2022-12-17 21:57:52 +0300

    few steps to finish

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ var stepSize = 20;
 
 function setup() {
   createCanvas(500, 500);
+ // rectMode(CENTER);
+  angleMode(DEGREES);
 }
 ///////////////////////////////////////////////////////////////////////
 function draw() {
@@ -13,8 +15,43 @@ function draw() {
 ///////////////////////////////////////////////////////////////////////
 function colorGrid(){
   // your code here
+ 
+  fill(255);
+  for(let i=0;i<width;i+=stepSize)
+  {
+    for(let j=0;j<height;j+=stepSize)
+  {
+     stroke(100);
+    var mouse=(width+1)/mouseX; 
+    var n=noise(i,j,frameCount/400*mouse);
+    var col=map(n,0,1,0,255);
+    var red=color(255,0,0);
+    var green=color(0,255,0);
+     
+   var lerp=lerpColor(red, green, n);
+  // fill(lerp);
+    rect(i,j,stepSize,stepSize);
+  }
+  }
+  
 }
 ///////////////////////////////////////////////////////////////////////
 function compassGrid(){
   // your code here
+ 
+  for(let i=0;i<height;i+=stepSize)
+  {
+    for(let j=0;j<width;j+=stepSize)
+  {
+     var mousy=(width+1)/mouseX; 
+    let noi=noise(j,i,frameCount/1000*mousy);
+    let val = map(noi,0,1,0,720);
+    
+    translate(j/2); // translate x to have value of j 
+    rotate(val);
+    stroke(255,0,0);
+    line(j,i,j,i+stepSize);
+    noStroke();
+  }
+  } 
 }
