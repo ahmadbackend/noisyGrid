9781b4ad9a3de08992bd2e8333963842d3c6faf6
ahmadbackend <ahmadbasherjsphp@gmail.com>
2022-12-25 06:57:04 +0300

    done

--- sketch.js
+++ sketch.js
@@ -22,13 +22,16 @@ function colorGrid(){
     for(let j=0;j<height;j+=stepSize)
   {
 
-    var mouse=width/mouseX+1; // to gurantee no division by zero 
-    var n=noise(i,j,frameCount/400*mouse);
+    var mouse=width/(mouseX+1); // to gurantee no division by zero 
+    var n;
+    //to avoid NAN when refresh as mouseX is undefined 
+   
+    n=noise(i/width,j/height,frameCount/200*mouse);
     var col=map(n,0,1,0,255);
     var red=color(125,0,0);
     var green=color(0,125,0);
-     
-   var lerp=lerpColor(red, green, n);
+    //combinate two colors based on n value(ratio between first and second color)
+    var lerp=lerpColor(red, green, n);
     fill(lerp);
     rect(i,j,stepSize,stepSize);
   }
@@ -39,33 +42,34 @@ function colorGrid(){
 var val;
 function compassGrid(){
   
- //push();
+
   for(let i=stepSize/2;i<height;i+=stepSize)
   {
   
-   //translate(0,i)
+  
   
     for(let j=stepSize/2;j<width;j+=stepSize)
   {
      
-    //x-coordinate in space=j 
-    //y-coordinatein space=i 
+    //x-coordinate in space=j divide by width for organic noise
+    //y-coordinatein space=i  divide by height for organic noise
     //both are large number  for noise 
    
     push(); 
     let noi=noise(j/width,i/height,frameCount/100);
+    
     val = map(noi,0,1,0,720);
     translate(j,i);
-    
-    
-    rotate(val*03);
-    
-    
-    stroke(255,0,0);
-    line(0,0,0,stepSize*0.9);
-   noStroke();
+    var speed=width/(mouseX+1); // as above to ensure no division by zero
+    // control compass speed by mouseX movement 
+    rotate(val*speed);
    
-   pop();
+    stroke(255,0,0);
+    //to generate different line length based on noise 
+    line(0,0,0,stepSize*noi);
+    noStroke();
+   
+     pop();
 
   }
  
